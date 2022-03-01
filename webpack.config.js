415fe67aeeb37a80037110be308ffaf5e3560e76
Mohammed Saif <saifmohammed888@gmail.com>
2022-03-01 15:41:17 +0530

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
             ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
         },
     },
+    module: {
+        rules: [
+            {
+                test: /\bmapbox-gl-csp-worker.js\b/i,
+                use: { loader: "worker-loader" },
+            },
+        ],
+    },
 }
